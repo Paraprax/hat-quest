21f28258fd683774efd7a93d699ee7a7fcca2aa9
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-07-07 19:37:21 -0400

    added example Field array for demoing

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+]);
+
 class Field {
     constructor(fieldArray) { //fieldArray will be an array of row-length arrays of the above turf and hole characters
 
