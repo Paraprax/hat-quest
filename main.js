397889d752b2add1bef85c9a75f0d14bbd1646dd
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-07-07 21:10:18 -0400

    print function now prints whole field row-by-row as strings

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
 const field1 = [
-    ['*', '░', 'O'],
-    ['░', 'O', '░'],
-    ['░', '^', '░'],
+  ["*", "░", "O"],
+  ["░", "O", "░"],
+  ["░", "^", "░"],
 ];
 
 class Field {
-    constructor(fieldArray) { //fieldArray will be an array of row-length arrays of the above turf and hole characters
-        this.fieldArray = fieldArray;
-    }
+  constructor(fieldArray) {
+    //fieldArray will be an array of row-length arrays of the above turf and hole characters
+    this.fieldArray = fieldArray;
+  }
 
-    print() {
-        for (var i = 0; i < this.fieldArray.length; i++) {
-            console.log(this.fieldArray[i]);
-        }
+  print() {
+    for (var i = 0; i < this.fieldArray.length; i++) {
+      let string = "";
+      for (var j = 0; j < this.fieldArray[i].length; j++) {
+        string += this.fieldArray[i][j];
+      }
+      console.log(string);
     }
+  }
 }
 
 const level1 = new Field(field1);
